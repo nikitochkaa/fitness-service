76c98d4343d916f801475f38fb3a2266ec4d7671
mykyta.onatskyi <nikitochkaaaaaa@gmail.com>
2024-06-09 13:52:38 +0300

    Cors configured

--- src/main/java/com/example/controller/AuthController.java
+++ src/main/java/com/example/controller/AuthController.java
@@ -5,15 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/auth")
 @Validated
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
   private final AuthService authService;
--- src/main/java/com/example/controller/MedicalRecordController.java
+++ src/main/java/com/example/controller/MedicalRecordController.java
@@ -3,15 +3,10 @@ package com.example.controller;
 import com.example.payload.response.MedicalRecordResponse;
 import com.example.service.MedicalRecordService;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/medical-records")
 @Validated
+@CrossOrigin(origins = "http://localhost:3000")
 public class MedicalRecordController {
 
   private final MedicalRecordService medicalRecordService;
